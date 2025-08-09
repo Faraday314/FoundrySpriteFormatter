248fc21402f5efc0502524c730f32525c55c9c52
Cole Savage <colesavage02@gmail.com>
2025-08-09 07:27:38 -0700

    Removed deprecated build comments and enabled the pyqt6 plugin in the actions script.

--- sprite_formatter.py
+++ sprite_formatter.py
@@ -1,11 +1,3 @@
-# Compilation mode, standalone everywhere, except on macOS there app bundle
-# nuitka-project-if: {OS} in ("Windows", "Linux", "FreeBSD"):
-#    nuitka-project: --onefile
-# nuitka-project-if: {OS} == "Darwin":
-#    nuitka-project: --standalone
-#    nuitka-project: --macos-create-app-bundle
-#
-
 import sys
 import os
 from math import floor
